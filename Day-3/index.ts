eaed797975c8e3604de6d94ffa9a4350c616c4de
Jawaid Ali <160029584+jawaidali735@users.noreply.github.com>
2024-04-26 18:12:04 +0500

    100 days of coding challenge

---
+++ Day-3/index.ts
@@ -0,0 +1,36 @@
+// Question 7:
+
+// addition
+console.log( 4 + 4 );
+
+// subtraction
+console.log( 10 - 2);
+
+// multiplication
+console.log( 2 * 4 );
+
+// division
+console.log( 16 / 2);
+
+// ___________________________________________________________________
+
+// question 8 :
+
+console.log( 4 + 4 );
+console.log('-' .repeat(20))
+console.log( 10 - 2);
+console.log('-' .repeat(20))
+console.log( 2 * 4 );
+console.log('-' .repeat(20))
+console.log( 16 / 2);
+console.log('-' .repeat(20))
+
+// _____________________________________________________________________
+
+
+// question 9:
+
+let favNumber = 7;
+
+console.log(`my fav number is: ${favNumber}`);
+
