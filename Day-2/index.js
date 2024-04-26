2c70b9f8f0373fc1ab86072e465ce8d0de906bfe
Jawaid Ali <160029584+jawaidali735@users.noreply.github.com>
2024-04-26 18:11:03 +0500

    100 days of coding challenge

---
+++ Day-2/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// Question 1:
+console.log("Bruce Lee once said, \"The successful warrior is the average man, with laser-like focus.\" ");
+// another way.
+console.log(`Bruce Lee once said, "The successful warrior is the average man, with laser-like focus."`);
+// Question 2:
+let famousPersonName = "Bruce Lee";
+let message = `${famousPersonName} once said, "The successful warrior is the average mam, with laser-like focus."`;
+console.log(message);
+// Question 3:
+let name1 = " \t\n jawaid ali \t\n";
+console.log(name1); //with white spaces
+console.log(name1.trim()); //white spaces removed with the help of trim()
---
+++ Day-2/index.ts
@@ -0,0 +1,25 @@
+// Question 1:
+
+console.log("Bruce Lee once said, \"The successful warrior is the average man, with laser-like focus.\" ");
+
+// another way.
+
+console.log(`Bruce Lee once said, "The successful warrior is the average man, with laser-like focus."`);
+
+// Question 2:
+
+let famousPersonName:string = "Bruce Lee";
+
+let message = `${famousPersonName} once said, "The successful warrior is the average mam, with laser-like focus."`
+
+console.log(message);
+
+
+// Question 3:
+
+let name1 = " \t\n jawaid ali \t\n";
+
+console.log(name1); //with white spaces
+
+console.log(name1.trim()); //white spaces removed with the help of trim()
+
