aea515e2ec2fb38c1ff44bdb00d6810f55d10aa1
Jawaid Ali <160029584+jawaidali735@users.noreply.github.com>
2024-04-26 18:13:52 +0500

    100 days of coding challenge

---
+++ Day-4/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// Question 10:
+// Jawaid Ali, 26-04-2024
+// last question is about store a favorite number and  print a message to showcase the favorite number.
+//question 11:
+let friendsName = ["ali", "mubeen", "areab", "saqib", "aamir"];
+for (let name of friendsName) {
+    console.log(name);
+}
+// we can also do this with foreach function. here is the example.
+friendsName.forEach((name) => {
+    console.log(name);
+});
+// question 12:
+let friendsNames = ["ali", "mubeen", "areab", "saqib", "aamir"];
+for (let name of friendsName) {
+    console.log(`Hello ${name}, I hope you are doing well.`);
+}
+// 
