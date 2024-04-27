2c84f1db851538050ab58cf2ad63fc558069644f
Jawaid Ali <160029584+jawaidali735@users.noreply.github.com>
2024-04-27 16:03:39 +0500

    100 days of coding challenge

---
+++ Day-9/index.ts
@@ -0,0 +1,88 @@
+// question 25:
+
+//first version
+
+let alienColor = "Green";
+
+if (alienColor == "Green"){
+    console.log("congratulations you have just earned 5 points");
+};
+
+//second version
+
+let alienColor2 = "Green";
+
+if (alienColor == "Yellow"){
+    
+};
+
+
+
+// Question 26:
+
+//if block will run
+
+if(alienColor == "Green"){
+    console.log("Congratulations you have just earned 10 points")
+}
+else{
+    console.log("Congratulations you have just earned 5 points")
+}
+
+//else block will run
+
+alienColor = "red"
+
+if(alienColor == "Green"){
+    console.log("Congratulations you have just earned 10 points")
+}
+else{
+    console.log("Congratulations you have just earned 5 points")
+}
+
+
+//Question 27:
+
+//first version 
+
+alienColor = "Green";
+
+if(alienColor == "Green"){
+    console.log("Congratulations you have just earned 20 points");
+}
+else if(alienColor == "Yellow"){
+    console.log("Congratulations you have just earned 15 points");
+}
+else if(alienColor == "red"){
+    console.log("Congratulations you have just earned 10 points");
+}
+
+
+//second version 
+
+alienColor = "Yellow";
+
+if(alienColor == "Green"){
+    console.log("Congratulations you have just earned 20 points");
+}
+else if(alienColor == "Yellow"){
+    console.log("Congratulations you have just earned 15 points");
+}
+else if(alienColor == "red"){
+    console.log("Congratulations you have just earned 10 points");
+}
+
+
+//third version 
+
+alienColor = "red";
+
+if(alienColor == "Green"){
+    console.log("Congratulations you have just earned 20 points");
+}
+else if(alienColor == "Yellow"){
+    console.log("Congratulations you have just earned 15 points");
+}
+else if(alienColor == "red"){
+    console.log("Congratulations you have just earned 10 points");
+}
