fd26910fcb1a9d9bb0954806d902d45c3081e8fc
Jawaid Ali <160029584+jawaidali735@users.noreply.github.com>
2024-04-27 15:55:00 +0500

    100 days of coding challenge

---
+++ Day-7/index.ts
@@ -0,0 +1,26 @@
+// Question 19: Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guestListInv = ["mubeen", "ali","javed","musk sahab", "areeb"];
+
+console.log(`Hello guys, our dinner going to be big bacuse i am invaitting: ${guestListInv.length}`);
+
+
+
+// Question 20: Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let cities = ["Turkey", "New York", "UAE", "Karachi", "swiss"];
+console.log("The places i want to visit",  cities)
+
+
+
+// Question 21: They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let car: {car: string; color: string; model:number; features:string} = {
+    car: "BMW",
+    color: "black",
+    model: 2020,
+    features: "automatic system etc"
+};
+
+
+console.log(`${car.car} with ${car.color} color and the model is ${car.model}, and comes with features ${car.features}`);
